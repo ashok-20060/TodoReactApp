969c113862593ff5286e80c894d7cd5480104ec0
AshokAgrawal <ashok20060@gmail.com>
2022-05-13 16:09:39 +0530

    ref added to inputbox....

--- todoapp/src/components/inputBox/InputBox.js
+++ todoapp/src/components/inputBox/InputBox.js
@@ -1,9 +1,17 @@
 import React from "react";
 import styles from "./inputBox.module.scss";
 export default class InputBox extends React.Component {
+  constructor(props) {
+    super(props);
+    this.inputRef = React.createRef();
+  }
+  componentDidMount() {
+    this.inputRef.current.focus();
+  }
   render() {
     return (
       <input
+        ref={this.inputRef}
         type="text"
         className={styles["input-text"]}
         placeholder="Enter new task for TodoList"
